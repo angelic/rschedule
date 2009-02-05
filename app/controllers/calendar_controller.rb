2fc7263b20f11524772f08cf7372c63b79131377
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-05 13:17:01 -0600

    working on week view

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,2 +1,6 @@
 class CalendarController < ApplicationController
+  def index
+    c = Calendar.first
+    @dates = c.dates.find_by_dates(Date.parse('2008-12-17')..Date.parse('2008-12-23'))
+  end
 end
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,2 +1,16 @@
 module CalendarHelper
+  def date_display
+    return unless @dates
+    from = @dates.first.value
+    to = @dates.size > 1 ? @dates.last.value : nil
+    
+    from_display = from.strftime("%b %d")
+    from_display << from.strftime(", %Y") unless to && from.year == to.year
+    to_display = ""
+    if to
+      to_display << to.strftime("%b ") unless from.month == to.month
+      to_display << to.strftime("%d, %Y")
+    end
+    "#{from_display} - #{to_display}"
+  end
 end
