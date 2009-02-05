d75ef2c31786149936325d8eaf05ca0d45aa9453
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-05 16:06:27 -0600

    events without times are each on their own row for each day

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,9 @@
 class CalendarController < ApplicationController
   def index
     c = Calendar.first
-    @dates = c.dates.find_by_dates(Date.parse('2008-12-17')..Date.parse('2008-12-23'))
+    range = Date.parse('2008-12-17')..Date.parse('2008-12-23')
+    @dates = c.dates.find_by_dates(range)
+    max = c.max_events_per_day_without_time_set(range)
+    @without_time_range = 0..(max - 1)
   end
 end
