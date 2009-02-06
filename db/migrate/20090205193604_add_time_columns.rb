aade6bd0cb8c99b440a5ef2b5ae3d20a45e7183d
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-05 20:44:11 -0600

    fix migration

--- db/migrate/20090205193604_add_time_columns.rb
+++ db/migrate/20090205193604_add_time_columns.rb
@@ -1,9 +1,11 @@
 class AddTimeColumns < ActiveRecord::Migration
   def self.up
-    add_column :calendar_events, :descr, :text
+    add_column :calendar_events, :start_time, :time
+    add_column :calendar_events, :end_time, :time
   end
 
   def self.down
-    remove_column :calendar_events, :descr
+    remove_column :calendar_events, :start_time
+    remove_column :calendar_events, :end_time
   end
 end
