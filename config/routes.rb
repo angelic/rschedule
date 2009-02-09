ce1bb2e21a887dbcdd18a2bd83bc653bcf266c1a
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-09 15:54:50 -0600

    small calendar updates main calendar with ajax

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ ActionController::Routing::Routes.draw do |map|
       :member => {:change_month => :post, :change_week => :post}
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "week"
+  map.root :controller => "weeks"
 
   # See how all your routes lay out with "rake routes"
 
