fc899e2f7e659bd4d1b45e346a9118959d76f19b
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-12 08:44:12 -0600

    dates are created if requested but don't exist

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -29,7 +29,7 @@ class WeeksController < ApplicationController
   protected
   
   def set_dates(range)
-    @dates = CalendarDate.by_dates(range)
+    @dates = CalendarDate.get_and_create_dates(range)
     max = @calendar.max_events_per_day_without_time_set(range)
     @without_time_range = 0..(max - 1)
     @selected_date = parse_date_or_now(params[:id]) 
