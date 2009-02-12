ff129d523ec85955efa6b3503ced830eef610b69
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-12 12:08:36 -0600

    added month to recurring events

--- db/migrate/20090209195005_add_schedule_fu_tables.rb
+++ db/migrate/20090209195005_add_schedule_fu_tables.rb
@@ -1,4 +1,4 @@
-class AddActsAsCalendarTables < ActiveRecord::Migration
+class AddScheduleFuTables < ActiveRecord::Migration
   def self.up
     create_table :calendars do |t|
       t.column :desc, :text
@@ -10,6 +10,7 @@ class AddActsAsCalendarTables < ActiveRecord::Migration
       t.column :weekday, :integer, :limit => 1, :null=>false
       t.column :monthweek, :integer, :limit => 1, :null=>false
       t.column :monthday, :integer, :limit => 1, :null=>false
+      t.column :month, :integer, :limit => 1, :null=>false
       t.column :lastweek, :integer, :limit => 1, :null=>false, :default=>0
       t.column :holiday, :boolean, :null=>false, :default=>false
     end
@@ -35,26 +36,29 @@ class AddActsAsCalendarTables < ActiveRecord::Migration
       t.column :weekday, :integer, :limit => 1
       t.column :monthweek, :integer, :limit => 1
       t.column :monthday, :integer, :limit => 1
+      t.column :month, :integer, :limit => 1
     end
  
     # FIXME - quote embedded holiday parameter
     execute "
 CREATE VIEW calendar_event_dates AS
 SELECT
-ce.id AS calendar_event_id,
-cd.id AS calendar_date_id
+  ce.id AS calendar_event_id,
+  cd.id AS calendar_date_id
 FROM calendar_dates cd
 INNER JOIN calendar_events ce ON cd.holiday = 'f' 
-AND (ce.start_date IS NULL OR cd.value >= ce.start_date)
-AND (ce.end_date IS NULL OR cd.value <= ce.end_date)
+  AND (ce.start_date IS NULL OR cd.value >= ce.start_date)
+  AND (ce.end_date IS NULL OR cd.value <= ce.end_date)
 LEFT OUTER JOIN calendar_occurrences co
-ON co.calendar_event_id = ce.id
-AND co.calendar_date_id = cd.id
+  ON co.calendar_event_id = ce.id
+  AND co.calendar_date_id = cd.id
 LEFT OUTER JOIN calendar_recurrences cr ON cr.calendar_event_id = ce.id
-AND ((cr.monthday IS NOT NULL AND cd.monthday = cr.monthday)
-OR (cr.monthday IS NULL AND cr.weekday IS NOT NULL
-AND cd.weekday = cr.weekday
-AND (cr.monthweek IS NULL OR cd.monthweek = cr.monthweek OR (cr.monthweek = -1 AND cd.lastweek = -1))
+  AND (cr.month IS NULL OR cr.month = cd.month)
+  AND ((cr.monthday IS NOT NULL AND cd.monthday = cr.monthday)
+  OR (cr.monthday IS NULL AND cr.weekday IS NOT NULL
+    AND cd.weekday = cr.weekday
+    AND (cr.monthweek IS NULL OR cd.monthweek = cr.monthweek 
+    OR (cr.monthweek = -1 AND cd.lastweek = -1))
 )
 )
 WHERE cr.id IS NOT NULL OR co.calendar_event_id IS NOT NULL
