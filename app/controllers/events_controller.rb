91ca6244cbd19594dc3a5a9c4900e45a8b361e56
Angel N. Sciortino <contact@angeliccomputing.com>
2009-02-12 16:29:50 -0600

    add event works for events that do not repeat

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   def create
-    @calendar_event = @calendar.events.create_for(params[:calendar_event][:start_date])
+    recurrence_type = params[:calendar_event].delete(:recurrence_type)
+    @calendar_event = @calendar.events.create_for(recurrence_type, params[:calendar_event])
     @calendar_event.update_attributes(params[:calendar_event])
     respond_to do |format|
       format.html { redirect_to(root_url) }
@@ -9,6 +10,7 @@ class EventsController < ApplicationController
   end
   
   def new
+    @calendar_event = CalendarEvent.new
     respond_to do |format|
       format.html
       format.js { render :layout => false }
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,10 @@
 module EventsHelper
+  def recurrence_types
+    [["Does not repeat", :norepeat], 
+        ["Weekdays", :weekdays], 
+        ["Daily", :daily],
+        ["Weekly", :weekly], 
+        ["Monthly", :monthly],
+        ["Yearly", :yearly]]
+  end
 end
